7fe2106ef3b1f7650c9540c0d507fc6aef922456
naumov <naumov.a.n@icloud.com>
2023-03-04 23:08:01 +0700

    выполнение задания со звёздочкой

---
+++ homework1.sql
@@ -0,0 +1,33 @@
+SELECT * FROM telephonebases.telephones;
+
+select product_name, manufacturer, price 
+from telephonebases.telephones
+where product_count > 2;
+
+select *
+from telephonebases.telephones
+where manufacturer = 'samsung';
+
+select *
+from telephonebases.telephones
+where product_name like '%iphone%';
+
+select *
+from telephonebases.telephones
+where manufacturer like '%samsung%';
+
+/*
+select *
+from telephonebases.telephones
+where ;
+*/
+
+select *
+from telephonebases.telephones
+where telephones.product_name like '%,8,%'
+   or telephones.product_name like '%,8'
+   or telephones.product_name like '8,%';
+/*
+4.3.* Товары, в названии которых есть ЦИФРЫ
+4.4.* Товары, в названии которых есть ЦИФРА "8"
+*/
